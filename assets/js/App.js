4906090250eb23375562e5cd42e42e193606c977
mafof <pavel.bykov@mafof.ru>
2019-07-17 22:49:41 +0300

    Remove state application

--- assets/js/App.js
+++ assets/js/App.js
@@ -8,12 +8,6 @@ class Application {
         this.appState = null; // Состояние приложения
         this.ipc = require('electron').ipcRenderer;
         this.timer = new Timer();
-        
-        this.MAIN_STATE     = 0; // При старте приложения
-        this.START_STATE    = 1; // При старте таймера
-        this.PAUSE_STATE    = 2; // При паузе таймера
-        this.CONTINUE_STATE = 3; // При возобновление таймера
-        this.STOP_STATE     = 4; // При остановке таймера
     }
 
     get amountTime() { return this.timer.amountTime; }
@@ -21,13 +15,6 @@ class Application {
     set amountTime(seconds) {
         this.timer.amountTime = seconds;
         this.timer.setTimeOut(this.timer.convertSecondsToTextTime(this.amountTime));
-    }
-
-    get state() { return this.appState; }
-
-    set state(value) {
-        if(typeof value === 'number' && value <= 4)
-            this.appState = value;
     }
 }
 
